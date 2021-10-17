c6466007776d4300faf3f8c124cd6fc7194716f0
Ratoriku <a1063021545@gmail.com>
2021-10-17 17:50:41 +0800

    clover: Remove custom fingerprint
    * We already have PixelPropUtils to pass SafetyNet etc.
    
    Signed-off-by: Ratoriku <a1063021545@gmail.com>
    Signed-off-by: pix106 <sbordenave@gmail.com>

--- lineage_clover.mk
+++ lineage_clover.mk
@@ -34,13 +34,6 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # Inherit device configuration
 $(call inherit-product, device/xiaomi/clover/device.mk)
 
-# Build Description
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="clover-user 8.1.0 OPM1.171019.019 9.7.4 release-keys"
-
-# Build Fingerprint
-BUILD_FINGERPRINT := "xiaomi/wayne/wayne:8.1.0/OPM1.171019.011/V9.5.11.0.ODCCNFA:user/release-keys"
-
 # Device identifier
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi

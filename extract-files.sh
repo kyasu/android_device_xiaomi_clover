ce84c3183fa017192c2c8c72d06a63e630d378f3
kyasu <kyasu.dev@gmail.com>
2023-12-10 22:58:55 +0900

    clover: libstdc++.vendor => libstdc++_vendor

--- extract-files.sh
+++ extract-files.sh
@@ -13,6 +13,9 @@ function blob_fixup() {
                 "${PATCHELF}" --add-needed "libcamera_sdm660_shim.so" "$LIBCAMERA_SDM660_SHIM"
             done
             ;;
+        vendor/lib/libFaceGrade.so | vendor/lib/libXMFD_AgeGender.so | vendor/lib/lib_lowlight.so | vendor/lib/libarcsoft_beautyshot.so | vendor/lib/libchromaflash.so | vendor/lib/libdualcameraddm.so | vendor/lib/libmmcamera_hdr_gb_lib.so | vendor/lib/liboptizoom.so | vendor/lib/libseemore.so | vendor/lib/libtrueportrait.so | vendor/lib/libts_detected_face_hal.so | vendor/lib/libts_face_beautify_hal.so | vendor/lib/libubifocus.so | vendor/lib/libvideobokeh.so | vendor/lib64/libchromaflash.so | vendor/lib64/libdualcameraddm.so | vendor/lib64/liboptizoom.so | vendor/lib64/libseemore.so | vendor/lib64/libts_detected_face_hal.so | vendor/lib64/libubifocus.so | vendor/lib64/libvideobokeh.so)
+            "${PATCHELF}" --replace-needed "libstdc++.so" "libstdc++_vendor.so" "${2}"
+            ;;
     esac
 }
 

6dfd645c1cc0e4348045786a574a84d1ce57ae24
Yumi Yukimura <me.cafebabe@gmail.com>
2024-06-25 00:19:16 +0800

    clover: Migrate to QTI USB Gadget 1.2 HIDL
    Change-Id: I6adbca2fab9a3fe0477d17f9786166828b786710

--- device.mk
+++ device.mk
@@ -62,7 +62,16 @@ PRODUCT_COPY_FILES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service.dual_role_usb
+    android.hardware.usb@1.3-service.dual_role_usb \
+    android.hardware.usb.gadget@1.2-service-qti
+
+PRODUCT_PACKAGES += \
+    init.qcom.usb.rc \
+    init.qcom.usb.sh \
+    usb_compositions.conf
+
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/qcom/opensource/usb/etc
 
 # Wifi
 PRODUCT_PACKAGES += \

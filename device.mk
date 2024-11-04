e0052e877fff109e8311982ea6ebbd78a2c30164
Yumi Yukimura <me.cafebabe@gmail.com>
2024-11-04 20:55:55 +0800

    clover: Switch to AIDL USB Gadget HAL
    Change-Id: Ic561f62588232449c3bc1e887fc68defea634ecd

--- device.mk
+++ device.mk
@@ -63,7 +63,7 @@ PRODUCT_COPY_FILES += \
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.dual_role_usb \
-    android.hardware.usb.gadget@1.2-service-qti
+    android.hardware.usb.gadget-service.qti
 
 PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
